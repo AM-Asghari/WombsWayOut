d30aa3d743af26563024693a62f6e4ee9df43125
mo <mosawer.asg@outlook.com>
2023-06-08 11:37:02 +0200

    added enemy FoV and player chasing

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -8,7 +8,11 @@ public class Test : MonoBehaviour
     NavMeshAgent agent;
     public Transform[] waypoints;
     int waypointIndex;
-    Vector3 target;
+
+    public Transform target;
+    Vector3 destination;
+    bool playerSeen = false;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -20,19 +24,49 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, target) < 1)
+        //if (Vector3.Distance(transform.position, target) < 0.1f)
+        //{
+        //IterateWaypointIndex();
+        //UpdateDestination();
+        //}
+        if (playerSeen == true)
         {
-            IterateWaypointIndex();
-            UpdateDestination();
-        }      
+            Chase();
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerSeen = true;
+            Debug.Log("I See You");
+        }
+    }
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerSeen = false;
+            Debug.Log("I Dont See You");
+        }
+    }
+
+    void Chase()
+    {
+        if (Vector3.Distance(destination, target.position) > 1.0f)
+        {
+            destination = target.position;
+            agent.destination = destination;
+        }
     }
 
     void UpdateDestination()
     {
-        target = waypoints[waypointIndex].position;
-        agent.SetDestination(target);
+        //target = waypoints[waypointIndex].position;
+        //agent.SetDestination(target);
     }
-    
+
     void IterateWaypointIndex()
     {
         waypointIndex++;
